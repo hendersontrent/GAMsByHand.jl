b2ed305fa0fbd96e07f9cc9ec6aa09493aa871fe
Trent Henderson <trent.henderson1@outlook.com>
2021-10-07 11:47:29 +1100

    Initial work

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # julia-gam
 Explores some generalised additive model (GAM) and related (polynomial spline) architecture by hand in Julia.
+
+## Notes
+
+This project largely aims to replicate in Julia the excellent work in [Generalised Additive Models](https://m-clark.github.io/generalized-additive-models/technical.html) by Michael Clark, which was originally written in R.
---
+++ gam-by-hand.jl
@@ -0,0 +1,129 @@
+#----------------------------------------
+# This script sets out to replicate some
+# of the code found at:  
+# https://m-clark.github.io/generalized-additive-models/technical.html
+#----------------------------------------
+
+#----------------------------------------
+# Author: Trent Henderson, 7 October 2021
+#----------------------------------------
+
+using Random, Distributions, GLM, Plots
+
+# Simulate data
+
+Random.seed!(123)
+x = rand(Uniform(0, 1), 500)
+μ = zeros(500)
+y = zeros(500)
+
+for i in 1:size(x, 1)
+    μ[i] = sin(2 * (4 * x[i] - 2)) + 2 * exp(-(16 ^ 2) * ((x[i] - .5) ^ 2))
+    y[i] = rand(Normal(μ[i][1], .3))
+end
+
+# Plot data
+
+gr()
+plot(x, y, seriestype = :scatter, legend = false, markeralpha = 0.4)
+
+#--------------- GAM routines ---------------
+
+#---------------
+# Generate knots
+#---------------
+
+knots = collect(0.0:0.1:0.9)
+l = 1
+
+# Generate polynomial splines
+
+SplineMatrix = Array{Float64}(undef, size(x, 1), size(knots, 1))
+
+for i in 1:size(x, 1)
+    for j in 1:size(knots, 1)
+        if x[i] >= knots[j]
+            SplineMatrix[i][j] = (x[i] - knots[j]) ^ l 
+        else
+            SplineMatrix[i][j] = 0.0
+        end
+    end
+end
+
+# Plot
+
+
+
+#-------------------
+# Fit a linear model
+# for each basis
+#-------------------
+
+lmMod = lm(@formula(y ~ .-1), SplineMatrix)
+
+# Pull out coefficients
+
+
+
+# Multiply each basis function by the coefficient
+
+
+
+# Plot
+
+
+
+#----------------------
+# Switch to polynomial
+# degree of 3 for cubic
+# basis spline-esque fit
+#----------------------
+
+# Functionalise all of the above and redo
+
+function PolynomialSplineFit(x::Array, y::Array, k::Int64 = 5, l::Int64 = 3, doPlot = false)
+
+    # Check knot argument
+
+    k < size(x, 1) || error("Number of knots `k` should be less than length of input `x`.")
+
+    # Do knot operations for basis functions
+    
+    knots = collect(range(minimum(x), stop = maximum(x), length = k))
+
+    SplineMatrix = Array{Float64}(undef, size(x, 1), size(knots, 1))
+
+    for i in 1:size(x, 1)
+        for j in 1:size(knots, 1)
+            if x[i] >= knots[j]
+                SplineMatrix[i][j] = (x[i] - knots[j]) ^ l 
+            else
+                SplineMatrix[i][j] = 0.0
+            end
+        end
+    end
+
+    # Fit linear model
+
+
+
+    # Pull out coefficients
+
+
+
+    # Multiply each basis function by the coefficient
+
+    
+
+    # Final returns
+
+    if doPlot == true
+        gr()
+        myPlot = plot(x, y, seriestype = :scatter, legend = false, markeralpha = 0.4)
+        return myPlot
+    else
+        return BasisMatix
+    end
+end
+
+PolynomialSplineFit(x, y, 9, 3, true)
