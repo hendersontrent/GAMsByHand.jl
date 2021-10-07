05cb977cc01c6eaea8def7f4f265721953e545fd
Trent Henderson <trent.henderson1@outlook.com>
2021-10-07 13:58:47 +1100

    proper function documentation

--- gam-by-hand.jl
+++ gam-by-hand.jl
@@ -79,13 +79,14 @@ display(myPlot)
 # function
 #-------------------
 
-# Fit model
+# Fit model and extract coefficients
 
 m₁ = lm(SplineMatrix, y)
+coefs = zeros(size(SplineMatrix, 2))
 
-# Pull out coefficients
-
-
+for i in 1:size(SplineMatrix, 2)
+    coefs[i] = GLM.coef(m₁)[i]
+end
 
 # Multiply each basis function by the coefficient
 
@@ -103,20 +104,52 @@ m₁ = lm(SplineMatrix, y)
 
 # Functionalise all of the above and redo
 
-function PolynomialSplineFit(x::Array, y::Array, k::Int64 = 5, l::Int64 = 3, doPlot = false)
+"""
+    FitPolynomialSpline(x, y, k, l, doPlot)
+
+    Compute a basic univariate polynomial spline fit to mimic the machinery of a generalised additive model (GAM) using basis functions composed of linear models over a given specification of knots and polynomial order.
+
+Usage:
+```julia-repl
+FitPolynomialSpline(x, y, k, l, doPlot)
+```
+
+Arguments:
+- `x` : Predictor variable.
+- `y` : Response variable.
+- `k` : Number of knots to use.
+- `l` : Order of the polynomial.
+- `doPlot` : Whether to plot the data or return the data matrix.
+"""
+function FitPolynomialSpline(x::Array, y::Array, k::Int64 = 5, l::Int64 = 3, doPlot = false)
 
     # Check knot argument
 
     k < size(x, 1) || error("Number of knots `k` should be less than length of input `x`.")
 
-    # Do knot operations for basis functions
+    #--------------------
+    # Do knot operations 
+    # for basis functions
+    #--------------------
+
+    # Genrate sequence of knots
     
     knots = collect(range(minimum(x), stop = maximum(x), length = k))
 
-    SplineMatrix = zeros(size(x, 1), size(k, 1))
+    # Instantiate matrix
+
+    SplineMatrix = zeros(size(x, 1), size(knots, 1) + 1)
+
+    # Add a column of ones for intercept
 
     for i in 1:size(SplineMatrix, 1)
-        for j in 1:size(SplineMatrix, 2)
+        SplineMatrix[i, size(SplineMatrix, 2)] = 1
+    end
+
+    # Fill remainder of the matrix
+
+    for i in 1:size(SplineMatrix, 1)
+        for j in 1:(size(SplineMatrix, 2) - 1)
             if x[i] >= knots[j]
                 SplineMatrix[i, j] = (x[i] - knots[j]) ^ l 
             else
@@ -125,19 +158,32 @@ function PolynomialSplineFit(x::Array, y::Array, k::Int64 = 5, l::Int64 = 3, doP
         end
     end
 
-    # Fit linear model
+    # Reorder columns to get intercept first
 
+    ordering = vcat(size(SplineMatrix, 2), 1:(size(SplineMatrix, 2) - 1))
+    SplineMatrix = SplineMatrix[:, ordering]
 
+    #---------------------
+    # Fit linear model and 
+    # extract coefficients
+    #---------------------
 
-    # Pull out coefficients
+    # Fit model
 
+    m₁ = lm(SplineMatrix, y)
+    coefs = zeros(size(SplineMatrix, 2))
 
+    for i in 1:size(SplineMatrix, 2)
+        coefs[i] = GLM.coef(m₁)[i]
+    end
 
     # Multiply each basis function by the coefficient
 
     
 
+    #--------------
     # Final returns
+    #--------------
 
     if doPlot == true
         gr()
@@ -148,4 +194,4 @@ function PolynomialSplineFit(x::Array, y::Array, k::Int64 = 5, l::Int64 = 3, doP
     end
 end
 
-PolynomialSplineFit(x, y, 9, 3, true)
+FitPolynomialSpline(x, y, 9, 3, true)
